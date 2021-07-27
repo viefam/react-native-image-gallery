b0ba7a062bb591a7a05e86b1c6cc823590b51cac
VietDQ <quocvietdang97@gmail.com>
2021-07-27 00:10:09 -0700

    fix warning

--- src/libraries/ViewPager/index.js
+++ src/libraries/ViewPager/index.js
@@ -260,7 +260,7 @@ export default class ViewPager extends PureComponent {
     }
 
     keyExtractor (item, index) {
-        return index;
+        return index && index.toString();
     }
 
     renderRow ({ item, index }) {
